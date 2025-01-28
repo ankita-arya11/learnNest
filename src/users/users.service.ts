9e31161532c09a886a558e37ce7ca5c5d95dd898
HIPL <hipl@macs-iMac-Pro.local>
2025-01-28 11:31:40 +0530

    working on dto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,88 +1,117 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  private users = [
-    [
-      {
-        id: 1,
-        name: 'Alice',
-        email: 'alice@example.com',
-        role: 'INTERN',
-      },
-      {
-        id: 2,
-        name: 'Bob',
-        email: 'bob@example.com',
-        role: 'ENGINEER',
-      },
-      {
-        id: 3,
-        name: 'Charlie',
-        email: 'charlie@example.com',
-        role: 'ADMIN',
-      },
-      {
-        id: 4,
-        name: 'David',
-        email: 'david@example.com',
-        role: 'INTERN',
-      },
-      {
-        id: 5,
-        name: 'Eve',
-        email: 'eve@example.com',
-        role: 'ENGINEER',
-      },
-      {
-        id: 6,
-        name: 'Frank',
-        email: 'frank@example.com',
-        role: 'ADMIN',
-      },
-      {
-        id: 7,
-        name: 'Grace',
-        email: 'grace@example.com',
-        role: 'INTERN',
-      },
-    ],
-  ];
-  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    if(role){
-        return this.users.filter(user => user.role === role)
-    }
-    return this.users
-  }
-
-  findOne(id: number){
-    const user = this.users.find(user => user.id === id)
-    return user;
-  }
-
-  create(user: { name: string, email:string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
-    const usersByHighestId = [...this.users].sort((a,b) => b.id = a.id) 
-    const newUser = {
-        id: usersByHighestId[0].id + 1,
-        ...user
-    }
-    this.users.push(newUser)
-    return newUser
-  }
-  update(id: number, updatedUser: { name?: string, email?:string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }){
-    this.users = this.users.map(user => {
-        if(user.id === id) {
-            return { ...user, ...updatedUser }
+    private users = [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "email": "Sincere@april.biz",
+            "role": "INTERN"
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "email": "Shanna@melissa.tv",
+            "role": "INTERN"
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "email": "Nathan@yesenia.net",
+            "role": "ENGINEER"
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "email": "Julianne.OConner@kory.org",
+            "role": "ENGINEER"
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "email": "Lucio_Hettinger@annie.ca",
+            "role": "ADMIN"
+        },
+        {
+            "id": 6,
+            "name": "Yaku",
+            "email": "yaku@gmail.com",
+            "role": "ENGINEER"
+        },
+        {
+            "id": 7,
+            "name": "Selena",
+            "email": "selena@gmail.com",
+            "role": "ENGINEER"
+        },
+        {
+            "id": 8,
+            "name": "Drake",
+            "email": "drake@gmail.com",
+            "role": "ENGINEER"
+        },
+        {
+            "id": 9,
+            "name": "Alex",
+            "email": "alex@gmail.com",
+            "role": "INTERN"
+        },
+        {
+            "id": 10,
+            "name": "Dave",
+            "email": "dave@davegray.codes",
+            "role": "ADMIN"
         }
+    ]
+
+    findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        if (role) {
+            const rolesArray = this.users.filter(user => user.role === role)
+            if (rolesArray.length === 0) throw new NotFoundException('User Role Not Found')
+            return rolesArray
+        }
+        return this.users
+    }
+
+    findOne(id: number) {
+        const user = this.users.find(user => user.id === id)
+
+        if (!user) throw new NotFoundException('User Not Found')
+
         return user
-    })
-    return this.findOne(id)
+    }
+
+    create(createUserDto: CreateUserDto) {
+        const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id)
+        const newUser = {
+            id: usersByHighestId[0].id + 1,
+            ...createUserDto
+        }
+        this.users.push(newUser)
+        return newUser
+    }
+
+    update(id: number, updateUserDto: UpdateUserDto) {
+        this.users = this.users.map(user => {
+            if (user.id === id) {
+                return { ...user, ...updateUserDto }
+            }
+            return user
+        })
+
+        return this.findOne(id)
+    }
 
     delete(id: number) {
         const removedUser = this.findOne(id)
-        this.users = this.users.filter(user => user.id! == id)
 
-        return removedUser;
+        this.users = this.users.filter(user => user.id !== id)
+
+        return removedUser
     }
-  }
+
 }
